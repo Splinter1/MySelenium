cb67056a1b66625d149af14b9579148c93884940
lizongzheng <zongzheng.li1@pactera.com>
2020-05-07 16:55:40 +0800

    加入ExtentReports框架美化报告

--- src/main/java/page/baidu/index.java
+++ src/main/java/page/baidu/index.java
@@ -3,6 +3,8 @@ package page.baidu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import util.action.Actions;
 import util.driver.SeleniumDrive;
@@ -10,14 +12,35 @@ import util.find.WebElementUtils;
 
 public class index {
 
-    @Test(priority = 0, description = "测试")
-    public void baidu()throws Exception{
-        WebDriver driver = SeleniumDrive.open();
-        driver.get("https://www.baidu.com/");
-        Actions.sendKeys(By.id("kw"),"测试");
+//    @Test(priority = 0, description = "测试")
+//    public void baidu()throws Exception{
+//        WebDriver driver = SeleniumDrive.open();
+//        driver.get("https://www.baidu.com/");
+//        Actions.sendKeys(By.id("kw"),"测试");
+//
+//        Thread.sleep(500);
+//        SeleniumDrive.closedAll();
+//    }
 
-        Thread.sleep(500);
-        SeleniumDrive.closedAll();
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void logDemo(){
+        Reporter.log("这是我们自己写的日志");
+        throw new RuntimeException("这是我自己的运行时异常");
     }
 
 
